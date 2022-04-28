b1a497b19ca311fe47fbcb72f1032ced308995db
Erik Yuzwa <erikyuzwa@gmail.com>
2022-04-28 14:58:27 -0600

    initial work

--- src/main/java/com/erikyuzwa/springboottodo/controllers/PageController.java
+++ src/main/java/com/erikyuzwa/springboottodo/controllers/PageController.java
@@ -1,14 +1,12 @@
 package com.erikyuzwa.springboottodo.controllers;
 
 import com.erikyuzwa.springboottodo.models.TodoItem;
-import com.erikyuzwa.springboottodo.models.TodoList;
 import com.erikyuzwa.springboottodo.repositories.TodoItemRepository;
-import com.erikyuzwa.springboottodo.repositories.TodoListRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
@@ -16,25 +14,21 @@ public class PageController {
     @Autowired
     private TodoItemRepository todoItemRepository;
 
-    @Autowired
-    private TodoListRepository todoListRepository;
-
     @GetMapping("/")
-    public ModelAndView getWelcome() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("welcome");
+    public String getWelcome(Model model) {
 
-        TodoList todoList = new TodoList();
         TodoItem todoItem = new TodoItem();
 
-        todoItem.setTodoList(todoList);
         todoItem.setCompleted(false);
         todoItem.setDescription("Hello World");
         
-        todoListRepository.save(todoList);
         todoItemRepository.save(todoItem);
 
-        return modelAndView;
+        Iterable<TodoItem> todoItems = todoItemRepository.findAll();
+
+        model.addAttribute("todoItems", todoItems);
+        
+        return "welcome";
     }
     
 }
